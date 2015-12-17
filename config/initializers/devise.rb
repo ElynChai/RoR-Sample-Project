123f90d3337b3a4232a803f6445eeeb6d98500c8
YOUR NAME <YOUR@EMAIL.com>
2015-12-17 13:18:03 +0000

    Deploy to heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '9dd91482d90785b510a80f981063245676703633db7e60021ba3953b6d86aa85abe2bf2ca69fad8ab65c748880a146e58276b67b6a48d0f0350cabd3de25abc3'
+ config.secret_key = '9dd91482d90785b510a80f981063245676703633db7e60021ba3953b6d86aa85abe2bf2ca69fad8ab65c748880a146e58276b67b6a48d0f0350cabd3de25abc3'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
